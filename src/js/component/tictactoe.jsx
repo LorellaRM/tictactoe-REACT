9311693ddb8143cb22e599b55d8c72062091b8dd
alexcastillla <alexander.zcas@gmail.com>
2020-10-25 20:02:03 +0000

    Corrected JS file

--- src/js/component/tictactoe.jsx
+++ src/js/component/tictactoe.jsx
@@ -11,58 +11,61 @@ export function Game() {
 
 export function Square(props) {
 	return (
-		<button className="squareClass" onClick={props.onClick}>
-			{" "}
+		<button className="square" onClick={props.onClick}>
 			{props.value}
 		</button>
 	);
 }
 
 export function Board() {
-	const [boardSquare, setBoardSquare] = useState(Array(9).fill(null));
-	const [xPlayer, setXPlayer] = useState(true);
+	const [boardSquares, setBoardSquares] = useState(Array(9).fill(null));
+	const [xIsNext, setXIsNext] = useState(true);
+
 	const handleClick = index => {
-		const squares = [...boardSquare];
-		if (calculateWinner(boardSquare) || squares[index]) return;
-		squares[index] = xPlayer ? "X" : "O";
-		setBoardSquare(squares);
-		setXPlayer(!xPlayer);
+		const squares = [...boardSquares];
+		if (calculateWinner(boardSquares) || squares[index]) return;
+
+		squares[index] = xIsNext ? "X" : "O";
+		setBoardSquares(squares);
+
+		setXIsNext(!xIsNext);
 	};
 
 	const renderSquare = index => {
 		return (
 			<Square
-				value={boardSquare[index]}
+				value={boardSquares[index]}
 				onClick={() => handleClick(index)}
 			/>
 		);
 	};
 
-	let status = xPlayer ? "X" : "O";
-	const winner = calculateWinner(boardSquare);
+	// let status;
+	// const winner = calculateWinner(boardSquares);
+	// status = winner ? { winner } : xIsNext ? "X" : "O";
 
-	function GetStatus() {
-		if (winner) {
-			return "The Winner is: " + winner;
-		} else {
-			return "Siguiente jugador: " + xPlayer;
-		}
+	const winner = calculateWinner(boardSquares);
+	let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = "Next player: " + (xIsNext ? "X" : "O");
 	}
 
 	return (
 		<div>
-			<div>
-				{" "}
+			<div className="status">{status}</div>
+			<div className="board-row">
 				{renderSquare(0)}
 				{renderSquare(1)}
 				{renderSquare(2)}
 			</div>
-			<div>
+			<div className="board-row">
 				{renderSquare(3)}
 				{renderSquare(4)}
 				{renderSquare(5)}
 			</div>
-			<div>
+			<div className="board-row">
 				{renderSquare(6)}
 				{renderSquare(7)}
 				{renderSquare(8)}
@@ -71,8 +74,8 @@ export function Board() {
 	);
 }
 
-function calculateWinner(squares) {
-	const winnerLines = [
+export function calculateWinner(squares) {
+	const winningLines = [
 		[0, 1, 2],
 		[3, 4, 5],
 		[6, 7, 8],
@@ -84,8 +87,8 @@ function calculateWinner(squares) {
 		[2, 4, 6]
 	];
 
-	for (let i = 0; i < winnerLines.length; i++) {
-		const [a, b, c] = winnerLines[i];
+	for (let i = 0; i < winningLines.length; i++) {
+		const [a, b, c] = winningLines[i];
 		if (
 			squares[a] &&
 			squares[a] === squares[b] &&
@@ -99,5 +102,5 @@ function calculateWinner(squares) {
 
 Square.propTypes = {
 	value: PropTypes.string,
-	onClick: PropTypes.string
+	onClick: PropTypes.func
 };
